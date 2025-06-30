952e0a8cb3335c900f6345a26c5c25107fad5a8a
nhancaon <caonhan.work@gmail.com>
2025-06-30 16:57:55 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,19 @@ import HomePage from './pages/HomePage';
 import About from './pages/AboutPage';
 import Input from './pages/InputPage';
 
-function Layout() {
-  return (
-    <>
-      <Header />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/input" element={<Input />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
-  );
-}
-
 function App() {
   return (
-    <Router basename="/Asian-Website">
+    <Router>
       <div className="flex flex-col min-h-screen">
-        <Routes>
-          <Route path="*" element={<Layout />} />
-        </Routes>
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/Asian-Website/input" element={<Input />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );

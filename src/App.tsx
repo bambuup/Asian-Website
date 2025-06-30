a5c39d2d2ec94e90d0a020181f7d2a76ff1a2c89
nhancaon <caonhan.work@gmail.com>
2025-06-30 16:55:14 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 import About from './pages/AboutPage';
 import Input from './pages/InputPage';
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/input" element={<Input />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
 function App() {
   return (
-    <Router>
+    <Router basename="/Asian-Website">
       <div className="flex flex-col min-h-screen">
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="*"
-            element={
-              <>
-                <Header />
-                <main className="flex-grow">
-                  <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/input" element={<Input />} />
-                  </Routes>
-                </main>
-                <Footer />
-              </>
-            }
-          />
+          <Route path="*" element={<Layout />} />
         </Routes>
       </div>
     </Router>
